ce07cf9b8aa62bdfbcc7799c21ff09d862e7e88c
Christian Schwarz <schwarz-chr@web.de>
2014-02-06 14:39:20 +0100

    added text to battery bar

--- battery.ruby
+++ battery.ruby
@@ -2,5 +2,6 @@
 
 s = `acpi`
 puts /([^ ]*)%/.match(s)[1].to_i / 100.0
+puts /%, (\d\d:\d\d)/.match(s)[1]
 puts "true" if /Discharging/.match s
 
--- mywidgets.lua
+++ mywidgets.lua
@@ -53,26 +53,29 @@ wicked.register(cpugraphwidget, wicked.widgets.cpu, '$1', 1, 'cpu')
 -- end cpu
 
 -- battery
-myBatteryWidget = widget({type = "imagebox"})
+myBatteryWidget = widget({type = "textbox"})
+myBatteryWidget.color = "#00FFFF"
 function batteryUpdate()
    local p = io.popen(incpath .. "battery.ruby")
    local value = tonumber(p:read())
-   local img = image.argb32(50, 20, nil)
+   local time = p:read()
+   local img = image.argb32(40, 20, nil)
 
    if (value > 1) then value = 1
    elseif (value < 0) then value = 0 
    end
    if (p:read()) then
-      c = "#FFFF00"
+      c = "#AFAF00"
    else
-      c = "#00FF00"
+      c = "#00AF00"
    end
-   img:draw_rectangle(0, 0, 50, 20, true, "#000000") --clear
-   img:draw_rectangle(0, 4, 50, 10, true, "#7F0000") --bg
-   img:draw_rectangle(0, 4, 50 * value, 10, true, c) --fg
-   myBatteryWidget.image = img
+   img:draw_rectangle(0, 0, 40, 16, true, "#000000") --clear
+   img:draw_rectangle(0, 0, 40, 16, true, "#7F0000") --bg
+   img:draw_rectangle(0, 0, 40 * value, 16, true, c) --fg
+   myBatteryWidget.bg_image = img
+   return {t = " " .. time, p = value * 100}
 end
 
-wicked.register(myBatteryWidget, batteryUpdate, nil, 60)
+wicked.register(myBatteryWidget, batteryUpdate, "<span color='#00FFFF'>$t</span>", 60)
 -- end battery
 
